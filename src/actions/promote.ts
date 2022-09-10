cff7f5206f4046e8c52c0d78f5e9c2f24a35e849
parroty <parroty@users.noreply.github.com>
2022-09-10 14:12:19 +0900

    Fix canary promote step

--- src/actions/promote.ts
+++ src/actions/promote.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import * as deploy from './deploy'
+import * as deploymentHelper from '../strategyHelpers/deploymentHelper'
 import * as canaryDeploymentHelper from '../strategyHelpers/canary/canaryHelper'
 import * as SMICanaryDeploymentHelper from '../strategyHelpers/canary/smiCanaryHelper'
 import {
@@ -73,7 +73,7 @@ async function promoteCanary(kubectl: Kubectl, manifests: string[]) {
       core.endGroup()
 
       core.startGroup('Deploying input manifests with SMI canary strategy')
-      await deploy.deploy(kubectl, manifests, DeploymentStrategy.CANARY)
+      await deploymentHelper.deployManifests(manifests, DeploymentStrategy.CANARY, kubectl, trafficSplitMethod)
       core.endGroup()
 
       core.startGroup('Redirecting traffic to stable deployment')
@@ -84,7 +84,7 @@ async function promoteCanary(kubectl: Kubectl, manifests: string[]) {
       core.endGroup()
    } else {
       core.startGroup('Deploying input manifests')
-      await deploy.deploy(kubectl, manifests, DeploymentStrategy.CANARY)
+      await deploymentHelper.deployManifests(manifests, DeploymentStrategy.CANARY, kubectl, trafficSplitMethod)
       core.endGroup()
    }
 
